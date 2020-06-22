034fa7a70c095dfb9429e060ad5e0817b9bca226
Joel Bender <jjb5@cornell.edu>
2020-06-22 02:01:53 -0400

    crude way to sort a ttl file

---
+++ samples/guideline-36-model-01/sort_turtle_file.py
@@ -0,0 +1,16 @@
+import sys
+
+lines = sys.stdin.readlines()
+
+chunks = []
+while lines:
+    blank_line_index = lines.index('\n')
+    chunks.append(lines[0:blank_line_index+1])
+    lines = lines[blank_line_index+1:]
+
+print(''.join(chunks[0][:-1]))
+del chunks[0]
+
+chunks.sort()
+for chunk in chunks:
+    print(''.join(chunk[:-1]))
