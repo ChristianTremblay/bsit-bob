2de7117aaee918cdf129c3664fe491dbd8603431
Joel Bender <jjb5@cornell.edu>
2021-01-05 02:18:29 -0500

    additional sample

---
+++ G36/Figure A-6.py
@@ -0,0 +1,144 @@
+"""
+Figure A-6
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+from bob import bind_model_namespace, dump
+
+from bob.core import System, Device, Part
+from bob.air import Fan, AirInlet, AirOutlet, AirConnection
+from bob.hw import HotWaterInlet, HotWaterOutlet
+from bob.signal import AnalogIn, AnalogOut, BinaryOut
+
+from header import g36_header
+
+__namespace__ = bind_model_namespace("ex", "urn:ex/")
+
+
+class AirFlowStation(Device):
+    airInlet: AirInlet
+    airOutlet: AirOutlet
+    flow = AnalogIn
+
+
+class DamperPositioner(Part):
+    position = AnalogOut
+
+
+class Damper(Device):
+    airInlet: AirInlet
+    airOutlet: AirOutlet
+    position: AnalogOut
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+        # create a positioner
+        self.damper_positioner = DamperPositioner(
+            label=self.label + ".damper_positioner"
+        )
+        self > self.damper_positioner
+
+        # reference the connections
+        self.position = self.damper_positioner.position
+
+
+class ValvePositioner(Part):
+    position = AnalogOut
+
+
+class HotWaterValve(Part):
+    pass
+
+
+class ECMFan(Fan):
+    # airInlet: AirInlet
+    # airOutlet: AirOutlet
+    pass
+
+
+class ECM(Part):
+    fanStart = BinaryOut
+    fanSpeedFeedback = AnalogIn
+    fanSpeedCommand = AnalogOut
+
+
+class HotWaterCoil(Device):
+    airInlet: AirInlet
+    airOutlet: AirOutlet
+    hwInlet: HotWaterInlet
+    hwOutlet: HotWaterOutlet
+    valvePosition: AnalogOut
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+        # create a positioner
+        self.valve_positioner = ValvePositioner(label=self.label + ".damper_positioner")
+        self > self.valve_positioner
+
+        # create a hot water valve
+        self.hot_water_valve = HotWaterValve(label=self.label + ".hot_water_valve")
+        self > self.hot_water_valve
+
+        # reference the properties
+        self.valvePosition = self.valve_positioner.position
+
+
+class VAV(System):
+    supplyAirInlet: AirInlet
+    returnAirInlet: AirInlet
+    supplyAirOutlet: AirOutlet
+    supplyAirFlow: AnalogIn
+    damperPosition: AnalogOut
+    hwInlet: HotWaterInlet
+    hwOutlet: HotWaterOutlet
+    hwValvePosition: AnalogOut
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+        # create an air flow station
+        self.air_flow_station = AirFlowStation(label=self.label + ".air_flow_station")
+        self > self.air_flow_station
+        self.supplyAirInlet >> self.air_flow_station.airInlet
+        self.supplyAirFlow = self.air_flow_station.flow
+
+        # create a damper
+        self.damper = Damper(label=self.label + ".damper")
+        self > self.damper
+        self.damperPosition = self.damper.position
+
+        # create a fan with an ECM part
+        self.fan = ECMFan(label=self.label + ".fan")
+        self.fan_ecm = ECM(label=self.label + ".fan.ecm")
+        self > self.fan > self.fan_ecm
+
+        # create a hot water coil
+        self.hot_water_coil = HotWaterCoil(label=self.label + ".hot_water_coil")
+        self > self.hot_water_coil
+        self.hwInlet >> self.hot_water_coil.hwInlet
+        self.hwOutlet << self.hot_water_coil.hwOutlet
+        self.hwValvePosition = self.hot_water_coil.valvePosition
+
+        # fan output goes to the hot water coil
+        self.fan >> self.hot_water_coil
+        self.hot_water_coil.airOutlet >> self.supplyAirOutlet
+
+        # connection for merge
+        merged_air = AirConnection(label=self.label + ".merge")
+
+        # link the air pieces together
+        self.returnAirInlet >> merged_air
+        self.damper >> merged_air
+        merged_air >> self.fan
+
+
+# make one
+vav = VAV(label="A-6")
+
+g36_header("figure006")
+dump()
