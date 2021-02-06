5c56bc9bf813ac48366c4bd0fd94bc2d8350075c
Joel Bender <jjb5@cornell.edu>
2021-02-06 14:12:09 -0500

    remove errant prefix line

--- samples/header.py
+++ samples/header.py
@@ -3,7 +3,6 @@ import sys
 SAMPLE_HEADER = """# baseURI: http://data.ashrae.org/standard223/1.0/sample/{sample_name}
 # imports: http://data.ashrae.org/standard223/1.0/model/all
 
-@prefix ex: <urn:ex/> .
 @prefix owl: <http://www.w3.org/2002/07/owl#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
