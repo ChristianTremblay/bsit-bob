afd3ccc1628db5eb769bc860010a53c710266881
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-02-06 22:19:33 -0500

    Providing airInlet and airOutlet to HVACZone and HVACSpace

--- bob/space/hvac.py
+++ bob/space/hvac.py
@@ -1,4 +1,11 @@
 from rdflib import URIRef
+
+from bob.connections.air import (
+    AirInletConnectionPoint,
+    AirInletZoneConnectionPoint,
+    AirOutletConnectionPoint,
+    AirOutletZoneConnectionPoint,
+)
 from ..core import DomainSpace, HVAC, s223, Zone
 
 __namespace__ = s223
@@ -6,7 +13,11 @@ __namespace__ = s223
 
 class HVACSpace(DomainSpace):
     hasDomain = HVAC
+    airInlet: AirInletConnectionPoint
+    airOutlet: AirOutletConnectionPoint
 
 
 class HVACZone(Zone):
     hasDomain = HVAC
+    airInlet: AirInletZoneConnectionPoint
+    airOutlet: AirOutletZoneConnectionPoint
