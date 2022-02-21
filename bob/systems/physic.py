15e1e874166264b334acb5553e5aae762720e568
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-02-21 08:31:22 -0500

    New subclasses of system for function block and physic systems. I use the mapsTo feature to abstract systems that could serve any goal like calcuating possible indoor air temp, etc... calculation would be made outside of s223, using those block as gateway

---
+++ bob/systems/physic.py
@@ -0,0 +1,84 @@
+"""
+A system is a portion of the universe that has been chosen for studying the changes that 
+take place within it in response to varying conditions. A system may be complex, such as a 
+planet, or relatively simple, as the liquid within a glass. Those portions of a system that 
+are physically distinct and mechanically separable from other portions of the system are called phases.
+
+[ref : https://www.britannica.com/science/phase-state-of-matter#ref507387]
+
+"""
+
+from rdflib import URIRef
+from ..connections.air import (
+    AirBidirectionalConnectionPoint,
+    AirInletConnectionPoint,
+    AirOutletConnectionPoint,
+    AirInletSystemConnectionPoint,
+    AirOutletSystemConnectionPoint,
+    AirBidirectionalSystemConnectionPoint,
+)
+from ..core import System, p223, s223
+from ..property import QuantifiableObservableProperty
+
+
+__namespace__ = p223
+
+
+class PhysicSystem(System):
+    """
+    This building block is an abstraction of a physic system
+    It is meant to semantically model the effect of various environmental variables
+    on a medium. (By opposition to mathematically model)
+
+    ex. Indoor air in a space may be affected and its properties changed
+    by opening a window or by using a baseboard heater.
+
+    This system models the variation of the properties that will be seen
+    in the medium of the object.
+
+    It works a little like a function block in the sense that it is
+    a black box that do not have to be defined in details (as all
+    the mathematic details are outside of the scope of the standard).
+    But it maps to devices and makes it clear that devices in a space, for
+    example, have an effect on the indoor air.
+
+    Unlike a typical system that contains devices, Physic Systems do not need
+    this. It model the intrinsec behaviour of the medium defined by the mapped connections
+
+    """
+
+    node_type = p223.PhysicSystem
+
+
+class IndoorAir(PhysicSystem):
+    """
+    This building block is an abstraction of a physic system
+    It is meant to model the effect of various environmental variables
+    on a medium.
+
+    ex. Indoor air in a space may be affected and its properties changed
+    by opening a window or by using a baseboard heater.
+
+    This system models the variation of the properties that will be seen
+    in the medium of the object.
+
+    IT works a little like a function block in the sense that it is
+    a black box that do not have to be defined in details (as all
+    the mathematic details are outside of the scope of the standard).
+    But it maps to devices and makes it clear that devices in a space, for
+    example, have an effect on the indoor air.
+
+    Unlike a typical system that contains devices, Physic Systems do not need
+    this. It model the intrinsec behaviour of the medium defined by the mapped connections
+
+    """
+
+    node_type = p223.PhysicSystem
+    hasMedium: URIRef = s223["Medium-Air"]
+    ductAirInlet: AirInletSystemConnectionPoint
+    ductAirOutlet: AirOutletSystemConnectionPoint
+    airTransfer: AirBidirectionalSystemConnectionPoint
+    doors: AirBidirectionalSystemConnectionPoint
+    windows: AirBidirectionalSystemConnectionPoint
+    radiantHeating: AirBidirectionalSystemConnectionPoint
+    radiantCooling: AirBidirectionalSystemConnectionPoint
