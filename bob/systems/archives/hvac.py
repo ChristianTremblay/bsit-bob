20b5ee5744da9d37c9eaa8d7ccc5573f1f361992
Joel Bender <joel@carrickbender.com>
2022-03-03 08:59:49 -0500

    missing modules not added earlier

---
+++ bob/systems/archives/hvac.py
@@ -0,0 +1,41 @@
+from ...core import s223, DomainSpace, Zone
+from ...connections.air import (
+    AirInletConnectionPoint,
+    AirOutletConnectionPoint,
+    AirInletZoneConnectionPoint,
+    AirOutletZoneConnectionPoint,
+)
+
+__namespace__ = s223
+
+
+class HVACZone(Zone):
+    """
+    An HVAC Zone with and supply and return air connection points.
+    """
+
+    supplyAir: AirInletZoneConnectionPoint
+    returnAir: AirOutletZoneConnectionPoint
+
+
+class HVACZone1(HVACZone):
+    """
+    HVAC Zone Type 1
+
+    A simple HVAC Zone with a single space.
+    """
+
+    node_type = None
+
+    def __init__(self, label: str) -> None:
+        super().__init__(label=label)
+
+        # there is a space that is the destination of the air
+        self.space = DomainSpace(label=label + ".space")
+
+        self.supplyAir.mapsTo = AirInletConnectionPoint(
+            self.space, label=label + ".space.supplyAir"
+        )
+        self.returnAir.mapsTo = AirOutletConnectionPoint(
+            self.space, label=label + ".space.returnAir"
+        )
