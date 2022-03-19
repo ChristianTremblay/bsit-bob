2750eff3b9f2d5aee76dafabc5fce5fdb8d13bd7
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-03-19 17:51:32 -0400

    Added properties to Fan to see if it's possible to use the actual code

--- bob/devices/hvac/fan.py
+++ bob/devices/hvac/fan.py
@@ -1,7 +1,9 @@
 from rdflib import URIRef
 from typing import Dict
+
+from bob.property import QuantifiableObservableProperty
 from ...connections.electricity import ElectricalInletConnectionPoint
-from ...core import s223, Device
+from ...core import s223, Device, quantitykind, unit
 
 
 from ...connections.air import AirInletConnectionPoint, AirOutletConnectionPoint
@@ -19,14 +21,40 @@ fan_template = {
 }
 """
 
+class RPM(QuantifiableObservableProperty):
+    hasQuantityKind: URIRef = quantitykind.AngularVelocity
+    unit: URIRef = unit["REV-PER-MIN"]
+
+class Amps(QuantifiableObservableProperty):
+    hasQuantityKind: URIRef = quantitykind.ElectricCurrent
+    unit: URIRef = unit.A
+
+class HP(QuantifiableObservableProperty):
+    hasQuantityKind: URIRef = quantitykind.Power
+    unit: URIRef = unit.HP
+
+class ElectricPower(QuantifiableObservableProperty):
+    hasQuantityKind: URIRef = quantitykind.Power
+    unit: URIRef = unit.KiloW
+
+class PowerFactor(QuantifiableObservableProperty):
+    hasQuantityKind: URIRef = quantitykind.PowerFactor
+    unit: URIRef = unit.UNITLESS
 
 class Fan(Device):
     node_type: URIRef = s223.Fan
     airInlet: AirInletConnectionPoint
     airOutlet: AirOutletConnectionPoint
     electricalInlet: ElectricalInletConnectionPoint  # can come from a VFD
+    amps: Amps
+    rpm: RPM
+    hp: HP
+    kw: ElectricPower
+    powerFactor: PowerFactor
 
     def __init__(self, config: Dict = None, **kwargs):
+        optional_properties = ["electricalInlet", "amps", "rpm", "hp", "powerFactor"]
+        _properties = {}
         if not config and not kwargs:
             raise ValueError(
                 "Please provide configuration dict or kwargs, at least a label"
@@ -34,8 +62,15 @@ class Fan(Device):
 
         sensors = define_sensors(config)
         devices, device_kwargs = contains_devices_list(config, **kwargs)
+        for each in optional_properties:
+            _properties[each] = device_kwargs.pop(each) if each in device_kwargs else None
 
         super().__init__(**device_kwargs)
+        for k,v in _properties.items():
+            try:
+                self.each = self.__annotations__[k](v)
+            except AttributeError:
+                self.each = self.__annotations__[k](self)
         for sensor in sensors:
             self > sensor
         for dev in devices:
