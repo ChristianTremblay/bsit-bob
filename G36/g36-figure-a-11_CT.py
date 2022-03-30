b947afd9a638e7d3bad07b4f17229fca95579820
Joel Bender <jjb5@cornell.edu>
2022-03-30 02:15:10 -0400

    devices cannot contain systems

--- G36/g36-figure-a-11_CT.py
+++ G36/g36-figure-a-11_CT.py
@@ -28,7 +28,7 @@ from bob.sensor.temperature import AirTemperatureSensor
 
 from bob.systems.hvac.g36 import G36Block, AnalogIn, AnalogOut, BinaryIn, BinaryOut
 
-from bob.core import clear, dump
+from bob.core import clear, dump, contains_mm, connect_mm
 
 from header import g36_header
 
@@ -57,6 +57,36 @@ vfd_template = {
     "sensors": {},
     "contains": {},
 }
+
+
+class HighStaticController(System):
+    highPressureNO: OnOffSignalSystemInletConnectionPoint
+    pushButton: OnOffSignalSystemInletConnectionPoint
+    pilotLight: LightVisibleOutletSystemConnectionPoint
+    enableVFD: OnOffSignalSystemOutletConnectionPoint
+    resetInput: OnOffSignalSystemInletConnectionPoint
+
+
+class VFDController(System):
+    enable: OnOffSignalSystemInletConnectionPoint
+    run: OnOffSignalSystemInletConnectionPoint
+    speed: ModulationSignalSystemConnectionPoint
+    status: OnOffSignalSystemOutletConnectionPoint
+    mstp: BidirectionalSystemConnectionPoint
+    bacnetIP: EthernetBidirectionalSystemConnectionPoint
+
+
+class FIG_A_11(G36Block):
+    sf_high_static_reset: BinaryOut
+    return_air_temp: AnalogIn
+    filter_dp: AnalogIn
+    hw_valve: AnalogOut
+    sf_status: BinaryIn
+    sf_speed: AnalogOut
+    sf_start: BinaryOut
+    supply_air_temp: AnalogIn
+    duct_static_press: AnalogIn
+    network: RS485BidirectionalSystemConnectionPoint
 
 
 # HVAC Side
@@ -102,28 +132,11 @@ dpt1.measure.hasExternalReference = BACnetReference(uri="bacnet://2/analog-input
 dpt2.measure.hasExternalReference = BACnetReference(uri="bacnet://2/analog-input/4")
 
 
-class HighStaticController(System):
-    highPressureNO: OnOffSignalSystemInletConnectionPoint
-    pushButton: OnOffSignalSystemInletConnectionPoint
-    pilotLight: LightVisibleOutletSystemConnectionPoint
-    enableVFD: OnOffSignalSystemOutletConnectionPoint
-    resetInput: OnOffSignalSystemInletConnectionPoint
-
-
 high_static = HighStaticController(
     label="SFHIGHSTATIC",
     comment="This system is the abstraction of control relay, push buttons and pilot light that are triggered by a high static pressure reading after the fan. The push button is the manual reset.",
 )
 high_static > dps
-
-
-class VFDController(System):
-    enable: OnOffSignalSystemInletConnectionPoint
-    run: OnOffSignalSystemInletConnectionPoint
-    speed: ModulationSignalSystemConnectionPoint
-    status: OnOffSignalSystemOutletConnectionPoint
-    mstp: BidirectionalSystemConnectionPoint
-    bacnetIP: EthernetBidirectionalSystemConnectionPoint
 
 
 vfd_controller = VFDController(
@@ -133,9 +146,11 @@ vfd_controller = VFDController(
 
 
 # Connections
+
 # Air
 vfd >> f
 ra >> filter >> hwc >> f >> sa
+
 # Water
 hws >> hwc.hotWaterInlet
 hwc.hotWaterOutlet >> hw_valve >> hwr
@@ -154,21 +169,6 @@ dpt2.hasMeasurementLocationHigh = sa
 dpt2.hasMeasurementLocationLow = inside
 
 # Map Systems
-
-
-class FIG_A_11(G36Block):
-    sf_high_static_reset: BinaryOut
-    return_air_temp: AnalogIn
-    filter_dp: AnalogIn
-    hw_valve: AnalogOut
-    sf_status: BinaryIn
-    sf_speed: AnalogOut
-    sf_start: BinaryOut
-    supply_air_temp: AnalogIn
-    duct_static_press: AnalogIn
-    network: RS485BidirectionalSystemConnectionPoint
-
-
 a11 = FIG_A_11(
     label="Figure-a-11",
     comment="This is a simple Fan Coil / Variable Volume. This system shows a fan controlled by a VFD to control static pressure in the supply air duct. The VFD will be disable on smoke detection, preventing the fan from running in case of fire. A high presure sensor will also disable the VFD and prevent the drive from running. In the latter case, a manual reset will be needed to authorize the VFD to restart. Supply air temeprature is maintained at setpoint by modulating a hot water valve (air pass through a hot water coil, before the fan). The discharge air temperature setpoint is calculated between limits to satisfied the demand created to maintain return air temeprature to setpoint.",
@@ -176,7 +176,8 @@ a11 = FIG_A_11(
 
 high_static.highPressureNO.mapsTo = dps.highStaticPressureOutput
 high_static.enableVFD.mapsTo = vfd_controller.enable
-vfd > vfd_controller
+
+# vfd > vfd_controller  ###TODO: devices cannot contain systems
 
 a11.return_air_temp.mapsTo = rat
 a11.supply_air_temp.mapsTo = dat
