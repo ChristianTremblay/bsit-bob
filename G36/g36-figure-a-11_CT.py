ae9afb843a8dc588f15cf85ab475c867a8ae2469
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-04-02 22:34:11 -0400

    Forgot to add a file

--- G36/g36-figure-a-11_CT.py
+++ G36/g36-figure-a-11_CT.py
@@ -133,13 +133,11 @@ dpt2 = DifferentialStaticPressureSensor(
 )
 
 # BACnet Stuff
-dps["DPS.sensor"].measure.hasExternalReference = BACnetReference(
-    uri="bacnet://2/binary-output/1"
-)
-rat.measure.hasExternalReference = BACnetReference(uri="bacnet://2/analog-input/1")
-dat.measure.hasExternalReference = BACnetReference(uri="bacnet://2/analog-input/2")
-dpt1.measure.hasExternalReference = BACnetReference(uri="bacnet://2/analog-input/3")
-dpt2.measure.hasExternalReference = BACnetReference(uri="bacnet://2/analog-input/4")
+dps["DPS.sensor"] @ BACnetReference(uri="bacnet://2/binary-output/1")
+rat @ BACnetReference(uri="bacnet://2/analog-input/1")
+dat @ BACnetReference(uri="bacnet://2/analog-input/2")
+dpt1 @ BACnetReference(uri="bacnet://2/analog-input/3")
+dpt2 @ BACnetReference(uri="bacnet://2/analog-input/4")
 
 
 high_static = HighStaticController(
