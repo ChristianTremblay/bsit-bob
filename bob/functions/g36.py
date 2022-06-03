449c13819d9e5ac91ca7bdaa37f97e207b8f18cd
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-06-02 22:23:46 -0400

    Should I create Official Function block for G36 figures.... would this be the way.... don't know yet

--- bob/functions/g36.py
+++ bob/functions/g36.py
@@ -52,3 +52,37 @@ class G36Sequence(FunctionBlock):
     """
 
     node_type = g36.FunctionBlock
+
+
+class G36Figure_A_1(G36Sequence):
+    def __init__(self, comment=None, **kwargs):
+        if not comment:
+            raise ValueError("Must provide sequence of operation as comment")
+        super().__init__(comment=comment, **kwargs)
+        self.zoneSetpointAdj = AnalogIn(
+            label="Zone Setpoint Adjust", function_block=self
+        )
+        self.LocalOverride = BinaryIn(label="Local Override", function_block=self)
+        self.zoneTemp = AnalogIn(label="Zone Temp", function_block=self)
+        self.zoneCO2 = AnalogIn(label="Zone CO2", function_block=self)
+        self.zonewindowSwitch = BinaryIn(
+            label="Zone Window Switch", function_block=self
+        )
+        self.zoneOccupancySensor = BinaryIn(
+            label="Zone Occupancy Sensor", function_block=self
+        )
+
+
+class G36Figure_A_3(G36Figure_A_1):
+    def __init__(self, comment=None, **kwargs):
+        super().__init__(comment=comment, **kwargs)
+
+
+class G36Figure_A_2(G36Figure_A_1):
+    def __init__(self, comment=None, **kwargs):
+        super().__init__(comment=comment, **kwargs)
+
+
+class G36Figure_A_10(G36Sequence):
+    def __init__(self, comment=None, **kwargs):
+        super().__init__(comment=comment, **kwargs)
