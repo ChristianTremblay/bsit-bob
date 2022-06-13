87669457c04615bd4b27c537b92de894db965b8f
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-06-12 22:42:31 -0400

    Current switch renamed to current relay. Switch will not be a good name...

--- bob/devices/electricity/switch.py
+++ bob/devices/electricity/switch.py
@@ -1,15 +1,80 @@
 from typing import Dict
 
-from bob.properties.states import OnOffStatus
+from bob.properties.electricity import Amps
+from bob.properties.states import OnOffCommand, OnOffStatus
 
 from ...connections.electricity import *
-from ...core import Device, Node, p223, s223
+from ...core import Device, Node, PropertyReference, logging, p223, s223
+from ...properties.time import Hour
 from ...sensor.electricity import CurrentBinarySensor
 
 _namespace = s223
 
+# TODO : Use templates
 
-class CurrentSwitch(Device):
+switch_template = {
+    "cp": {
+        "electricalInlet": Electricity_120V_60HzInletConnectionPoint,
+        "electricalOutlet": Electricity_120V_60HzOutletConnectionPoint,
+    },
+    "properties": {
+        ("amps", Amps): {},
+    },
+}
+
+
+class Switch(Device):
+    _class_iri = s223.ElectricalSwitch
+    # electricalInlet: ElectricalInletConnectionPoint
+    # electricalOutlet: ElectricalOutletConnectionPoint
+    hasMaxRange: Amps
+
+    def __init__(self, config: Dict = None, **kwargs):
+        _config = switch_template
+        if config:
+            _config.update(config)
+        kwargs = {**_config.get("params", {}), **kwargs}
+
+        super().__init__(_config, **kwargs)
+
+
+class SinglePoleSwitch(Switch):
+    """
+    One inlet and one outlet
+    hasMaxRange = current max of switch
+    A rule could check inlet and outlet are same class
+    """
+
+    _cross_ref = {
+        "120": (
+            Electricity_120V_60HzInletConnectionPoint,
+            Electricity_120V_60HzOutletConnectionPoint,
+        ),
+        "277": (
+            Electricity_277V_60HzInletConnectionPoint,
+            Electricity_277V_60HzOutletConnectionPoint,
+        ),
+        "347": (
+            Electricity_347V_60HzInletConnectionPoint,
+            Electricity_347V_60HzOutletConnectionPoint,
+        ),
+    }
+
+    def __init__(self, config: Dict = {}, **kwargs):
+        _config = switch_template
+        if config:
+            _config.update(config)
+        _voltage = kwargs.pop("voltage") if "voltage" in kwargs else None
+        if _voltage:
+            _electricalInlet, _electricalOutlet = self._cross_ref[str(_voltage)]
+            _config["cp"]["electricalInlet"] = _electricalInlet
+            _config["cp"]["electricalOutlet"] = _electricalOutlet
+        kwargs = {**_config.get("params", {}), **kwargs}
+
+        super().__init__(_config, **kwargs)
+
+
+class CurrentRelay(Device):
     """
     Current detection device that gives a OnOff status by the action
     of a dry contact when electricity is detected.
@@ -18,7 +83,7 @@ class CurrentSwitch(Device):
 
     """
 
-    _class_iri = s223.CurrentSwitch
+    _class_iri = s223.CurrentRelay
     outputSignal: OnOffSignalOutletConnectionPoint
     onOffStatus: OnOffStatus
 
@@ -39,3 +104,24 @@ class CurrentSwitch(Device):
         self.onOffStatus = sensor.observesProperty
         self._sensors = [sensor]
         self > sensor
+
+
+class TimerSwitch(SinglePoleSwitch):
+    """
+    Manuel switch with integrated timer
+    Typically used for exhaust fan in bathrooms for example
+    """
+
+    delay: Hour
+    # onOffStatus: PropertyReference
+    # onOffCommand: OnOffCommand
+
+    def __init__(self, config: Dict = {}, **kwargs):
+        _config = switch_template
+        if config:
+            _config.update(config)
+        _delay = kwargs.pop("delay") if "delay" in kwargs else None
+        if _delay:
+            _config["properties"][("delay", Hour)] = {"hasValue": _delay}
+        kwargs = {**_config.get("params", {}), **kwargs}
+        super().__init__(_config, **kwargs)
