d64c0bfba2c95f0e679ca59fe008742166074828
Joel Bender <joel@betelgeuse>
2022-07-08 09:06:25 -0400

    occupancy status is 'owned' by the space and 'inherited' by the zone, in this example every space has an occupancy status (not always true) and every zone has only one space (also not always true)

--- samples/sample_pritoni/lighting_spaces.py
+++ samples/sample_pritoni/lighting_spaces.py
@@ -12,57 +12,69 @@ _namespace = bind_model_namespace(model_name, f"urn:ex/{model_name}/")
 openofficeNorth_lightspace = LightingSpace(
     label="LightingSpace1", comment="OpenOffice North.Light"
 )
+openofficeNorth_lightspace.occupancy = OccupancyStatus()
+
 openofficeSouth_lightspace = LightingSpace(
     label="LightingSpace2", comment="OpenOffice South.Light"
 )
+openofficeSouth_lightspace.occupancy = OccupancyStatus()
+
 bathroom_lightspace = LightingSpace(label="LightingSpace3", comment="Bathroom.Light")
+bathroom_lightspace.occupancy = OccupancyStatus()
+
 corridor_lightspace = LightingSpace(label="LightingSpace5", comment="Corridor.Light")
+corridor_lightspace.occupancy = OccupancyStatus()
+
 privateoffice_lightspace = LightingSpace(
     label="LightingSpace4", comment="PrivateOffice.Light"
 )
+privateoffice_lightspace.occupancy = OccupancyStatus()
+
 kitchenette_lightspace = LightingSpace(
     label="LightingSpace6", comment="Kitchenette.Light"
 )
+kitchenette_lightspace.occupancy = OccupancyStatus()
 
 # Lighting Zones
 lighting_zone_1 = LightingZone(
     label="LightingZone1",
     comment="Contains OpenOffice Space North",
-    occupancy=OccupancyStatus(),
+    occupancy=openofficeNorth_lightspace.occupancy,
 )
 lighting_zone_1 > openofficeNorth_lightspace
+
 lighting_zone_2 = LightingZone(
     label="LightingZone2",
     comment="Contains OpenOffice Space South",
-    occupancy=OccupancyStatus(),
+    occupancy=openofficeSouth_lightspace.occupancy,
 )
 lighting_zone_2 > openofficeSouth_lightspace
 
 lighting_zone_3 = LightingZone(
     label="LightingZone3",
     comment="Contains Bathroom Light Space",
-    occupancy=OccupancyStatus(),
+    occupancy=bathroom_lightspace.occupancy,
 )
 lighting_zone_3 > bathroom_lightspace
 
 lighting_zone_4 = LightingZone(
     label="LightingZone4",
     comment="Contains Private Office Light Space",
-    occupancy=OccupancyStatus(),
+    occupancy=privateoffice_lightspace.occupancy,
 )
 lighting_zone_4 > privateoffice_lightspace
 
 lighting_zone_5 = LightingZone(
     label="LightingZone5",
     comment="Contains Corridor Light Space",
-    occupancy=OccupancyStatus(),
+    occupancy=corridor_lightspace.occupancy,
 )
 lighting_zone_5 > corridor_lightspace
 
 lighting_zone_6 = LightingZone(
     label="LightingZone6",
     comment="Contains Kitchenette Light Space",
-    occupancy=OccupancyStatus(),
+    occupancy=kitchenette_lightspace.occupancy,
 )
 lighting_zone_6 > kitchenette_lightspace
 
