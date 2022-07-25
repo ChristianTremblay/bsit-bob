9797efd6aec3e0721b54f2dbace4ce88f48e4c82
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-07-25 13:49:50 -0400

    Function was not targetting the actuator command

--- G36/g36-figure-a-1.py
+++ G36/g36-figure-a-1.py
@@ -28,6 +28,7 @@ from bob.core import (
     dump,
     QUANTITYKIND,
     UNIT,
+    G36,
 )
 from bob.devices.architectural import Window
 from bob.devices.hvac.damper import ElectricalActuatedProportionalDamper
@@ -178,7 +179,7 @@ vav = VAV_FIGA1(config=vav_system_template)
 supply_air >> vav["DPR"].airInlet
 
 vav["DPR"].airOutlet >> discharge_air >> hvac_space.ductAirInlet
-vav["DPR"]["actuator"].proportional_signal << controller.damper_output
+vav["DPR"]["actuator"]['proportional_signal'] << controller.damper_output
 vav["ZONE-THERMOSTAT"]["temperature_sensor"].hasMeasurementLocation = hvac_space
 vav["ZONE-THERMOSTAT"].mstp << controller.bacnet_mstp
 vav["ZN-CO2"]["CO2"].hasMeasurementLocation = hvac_space
@@ -231,6 +232,9 @@ g36fig_a_1.uses(hvac_zone.temperature_setpoint, AnalogInput, "zoneTemperatureSet
 g36fig_a_1.uses(hvac_zone.temperature, AnalogInput, "zoneTemperature")
 g36fig_a_1.uses(hvac_zone.co2, AnalogInput, "zoneTemperature")
 g36fig_a_1.uses(hvac_zone.windows_switch, BinaryInput, "window-switch")
-g36fig_a_1.produces(vav["damperPosition"], AnalogOutput, "damperPosition")
+g36fig_a_1.produces(vav["DPR"]['actuator']['command'], AnalogOutput, "damperPosition")
+
+controller.executes(occupancy)
+controller.executes(g36fig_a_1)
 
 dump(filename=f"G36/ttl/{model_name}.ttl", header=g36_header(model_name))
