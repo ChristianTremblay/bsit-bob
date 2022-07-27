5d749a98c2f54b1ffbdcba43f1e819a97a1912d5
Christian Tremblay, ing <christian.tremblay@servisys.com>
2022-07-26 22:17:45 -0400

    Alias for connections in the context of a controller

---
+++ bob/devices/control/__init__.py
@@ -0,0 +1,5 @@
+# Controller
+from ...connections.electricity import ModulationSignalInletConnectionPoint as AnalogInput
+from ...connections.electricity import ModulationSignalOutletConnectionPoint as AnalogOutput
+from ...connections.electricity import OnOffSignalInletConnectionPoint as BinaryInput
+from ...connections.electricity import OnOffSignalOutletConnectionPoint as BinaryOutput
