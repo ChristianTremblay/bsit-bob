8828e3dcdecee3e23ed712d59bb4ab72f7a9a408
Joel Bender <jjb5@cornell.edu>
2023-03-20 22:52:47 -0400

    a small bit of documentation

--- check-install.sh
+++ check-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#
+#   This script loops through the available Python versions and dumps out
+#   the installed version of Bob, if there is one.  This is handy when there
+#   are multiple installations floating around, maybe from different paths.
+#
+
 for version in 3.7 3.8 3.9 3.10;
 do
 if [ -a "`which python$version`" ]; then
