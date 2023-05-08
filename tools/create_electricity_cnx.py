8eb223ebf979b1b0e32cb4ebfdf4a5155b01ea46
Christian Tremblay, ing <christian.tremblay@servisys.com>
2023-05-07 21:56:54 -0400

    Created a script that read VOCAB enum to find electricity media to create media and all various connection points classes

--- tools/create_electricity_cnx.py
+++ tools/create_electricity_cnx.py
@@ -1,3 +1,19 @@
+from rdflib import Graph
+
+g = Graph()
+nodes = {}
+
+
+def parse_rdf(ttl_file):
+    global g
+    with open(ttl_file, "r") as ttl:
+        lines = ttl.read()
+    graph = g.parse(data=lines, format="turtle")
+    return graph
+
+
+g = parse_rdf("VOCAB_SP223_enumeration-v1.0.ttl")
+
 title = None
 details = None
 class_name = None
@@ -23,18 +39,57 @@ _namespace = BOB
 """
 
 
-template = """
+# title, details, class name, medium
+
+lst = set()
+for s, p, o in g:
+    try:
+        if ("Electricity-AC" in o or "Electricity-DC" in o) and "ns#type" in p:
+            print(s, p, o)
+            _title = s.split("#")[1]
+            if "Electricity-AC" in o:
+                _details = "3 Phases" if "3Ph" in _title else "1 phase"
+                _class_name = _title.split("AC-")[1].replace("-", "_").replace(".", "r")
+                _medium_suffix = _title.split("AC-")[1]
+                _ACDC = "AC"
+            else:
+                _details = "DC"
+                _class_name = _title.split("DC-")[1].replace("-", "_").replace(".", "r")
+                _medium_suffix = _title.split("DC-")[1]
+                _ACDC = "DC"
+
+            cn = f"Electricity_{_class_name}"
+            _medium = f"Electricity.{_ACDC}{_class_name}"
+            # print(title, details, class_name, medium)
+            lst.add((_title, _details, cn, _medium, _medium_suffix, _ACDC))
+    except Exception as error:
+        print(s, p, o)
+
+print(lst)
+print(f"There are {len(lst)} elements")
+with open("new_electricity.py", "w") as file:
+    file.write(header)
+
+    for each in sorted(lst):
+        title, details, class_name, medium, _, _ = each
+        for element in [
+            ("{title}", title),
+            ("{details}", details),
+            ("{class_name}", class_name),
+            ("{medium}", medium),
+        ]:
+            template = f"""
 # === {title}
 # {details}
 
 
 class {class_name}Connection(Connection):
-    hasMedium = Electricity.{medium}
+    hasMedium = {medium}
     _class_iri = S223.Connection
 
 
 class {class_name}ConnectionPoint(ConnectionPoint):
-    hasMedium = Electricity.{medium}
+    hasMedium = {medium}
 
 
 class {class_name}InletConnectionPoint(
@@ -50,7 +105,7 @@ class {class_name}OutletConnectionPoint(
 
 
 class {class_name}SystemConnectionPoint(SystemConnectionPoint):
-    hasMedium = Electricity.{medium}
+    hasMedium = {medium}
 
 
 class {class_name}SystemInletConnectionPoint(
@@ -64,39 +119,18 @@ class {class_name}SystemOutletConnectionPoint(
 ):
     _class_iri = BOB.OutletSystemConnectionPoint"""
 
-# title, details, class name, medium
-lst = [
-    ["575V_60Hz", "3 phases", "Electricity_575V_60Hz", "Electricity.AC575_60Hz"],
-    [
-        "575V_60Hz",
-        "1 phases",
-        "Electricity_575V_1Ph_60Hz",
-        "Electricity.AC600V1Ph_60Hz",
-    ],
-]
-
-
-def generate(title, details, class_name, medium):
-    global template
-    title = title
-    details = details
-    class_name = class_name
-    medium = medium
-    # print(template)
-    return template
-
-
-with open("new_electricity.py", "a") as file:
-    file.write(header)
-
-    for each in lst:
-        title, details, class_name, medium = each
-        for each in [
-            ("{title}", title),
-            ("{details}", details),
-            ("{class_name}", class_name),
-            ("{medium}", medium),
-        ]:
-            _t, _n = each
+            _t, _n = element
             template = template.replace(_t, _n)
-        file.write(generate(title, details, class_name, medium))
+        file.write(template)
+with open("add_to_media.py", "w") as file:
+    for each in sorted(lst):
+        title, details, class_name, medium, suffix, _ACDC = each
+        for element in [
+            ("{medium}", medium),
+            ("{suffix}", suffix),
+        ]:
+            template = f"""
+{medium} = Electricity.{_ACDC}("{suffix}")"""
+            _t, _n = element
+            template = template.replace(_t, _n)
+        file.write(template)
