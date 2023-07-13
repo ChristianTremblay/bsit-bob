b1d67aad663dcb090410b15cb203fc803b1c104e
Christian Tremblay, ing <christian.tremblay@servisys.com>
2023-07-12 23:01:23 -0400

    Make the bacnet object ready if present value is needed to be used as a external reference.

--- bob/bacnet.py
+++ bob/bacnet.py
@@ -11,6 +11,7 @@ from rdflib import XSD, Literal, URIRef
 
 from .core import Equipment, ExternalReference, Node, bind_namespace, INCLUDE_INVERSE
 from .equipment.control.controller import Controller
+from .externalreference.bacnet import BACnetExternalReference
 from .multimethods import multimethod, new_class
 
 # logging
@@ -24,9 +25,10 @@ _namespace = BACNET
 class Device(Controller):
     _class_iri: URIRef = BACNET.Device
     _namespace = BACNET
+    _device_object = Node
+    _bacnet_objects = set()
 
-
-class Object(ExternalReference):
+class Object(Node):
     _attr_uriref = {
         "objectIdentifier": BACNET["object-identifier"],
         "objectName": BACNET["object-name"],
@@ -37,13 +39,29 @@ class Object(ExternalReference):
     objectName: Literal
     objectType: URIRef
     description: Literal
+    ext_ref_args: list
 
+    def create_external_reference_url(self, bacnet_device, propertyIdentifier="present-value"):
+        _dev_instance = bacnet_device._device_object.objectIdentifier.split(',')[1]
+        _label = f"dev_{_dev_instance}.{self.objectName}"
+        _url = f"bacnet://{_dev_instance}/{self.objectIdentifier}/{propertyIdentifier}"
+        self.ext_ref_args = [_label, _url]
+
+    @property
+    def present_value(self):
+        "Creates the present_value on demand to be used as external reference for a property"
+        _label, _url = self.ext_ref_args
+        return BACnetExternalReference(_url, label=_label)
 
 @multimethod
 def contains_mm(device_: Device, object_: Object) -> None:
     """Device > Object"""
     _log.info(f"device {device_} contains object {object_}")
-
+    if isinstance(object_, DeviceObject):
+        device_._device_object = object_
+    else:
+        device_._bacnet_objects.add(object_)
+        object_.create_external_reference_url(device_)
     device_._data_graph.add((device_._node_iri, BACNET.hasObject, object_._node_iri))
     if INCLUDE_INVERSE:
         device_._data_graph.add(
