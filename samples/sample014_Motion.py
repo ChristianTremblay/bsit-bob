0fdbd268c54a4e7b547e41c8ee6c9eafba4b89dc
Joel Bender <jjb5@cornell.edu>
2024-01-16 01:05:17 -0500

    occupancy needs updating

--- samples/sample014_Motion.py
+++ samples/sample014_Motion.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
+from bob.core import bind_model_namespace, data_graph, schema_graph, dump
 from header import sample_header
 
-from bob.core import DomainSpace, PhysicalSpace, bind_model_namespace, dump
+from bob.core import DomainSpace, PhysicalSpace
 from bob.property import EnumeratedObservableProperty, QuantifiableObservableProperty
 
 from bob.enum import HVAC
@@ -17,5 +18,10 @@ space = HVACSpace(label="Room 101")
 ms = OccupantMotionSensor(hasObservationLocation=space, label="Motion Sensor")
 pc = OccupantCounterSensor(hasObservationLocation=space, label="People Counter")
 
-
-dump(filename=f"samples/ttl/{model_name}.ttl", header=sample_header(model_name))
+# dump the result
+dump(
+    data_graph,
+    filename=f"samples/ttl/{model_name}.data.ttl",
+    header=sample_header(model_name),
+)
+dump(schema_graph, filename=f"samples/ttl/{model_name}.schema.ttl")
