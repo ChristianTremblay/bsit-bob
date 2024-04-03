05ddfc5fad820ff17900185f1ef0c829617e1619
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-04-02 22:02:57 -0400

    relative imports. Bob Fan is nothing else than a fan... with a generic electrical inlet.

--- bob/equipment/hvac/fan.py
+++ bob/equipment/hvac/fan.py
@@ -3,13 +3,13 @@ from typing import Dict
 
 from rdflib import URIRef
 
-from bob.equipment.electricity.vfd import VFD
-from bob.properties.flow import Flow
-from bob.properties.ratio import Percent, PercentCommand
+from ...equipment.electricity.vfd import VFD
+from ...properties.flow import Flow
+from ...properties.ratio import Percent, PercentCommand
 
 from ...connections.air import AirInletConnectionPoint, AirOutletConnectionPoint
 from ...connections.electricity import (
-    Electricity_600VLL_3Ph_60HzInletConnectionPoint,
+    ElectricalInletConnectionPoint,
     Electricity_600VLL_3Ph_60HzOutletConnectionPoint,
 )
 from ...core import (
@@ -34,125 +34,22 @@ _log = logging.getLogger(__name__)
 # namespace
 _namespace = BOB
 
-fan_template = {
-    "cp": {"electricalInlet": Electricity_600VLL_3Ph_60HzInletConnectionPoint},
-    "properties": {
-        ("speedRatio", PercentCommand): {},
-        ("staticPressure", Pressure): {"hasUnit": UNIT.PA},
-        ("amps", Amps): {},
-        ("rpm", RPM): {},
-        ("cfm", Flow): {"hasUnit": UNIT["FT3-PER-MIN"]},
-        ("hp", HP): {},
-        ("kW", ElectricPowerkW): {},
-        ("powerFactor", PowerFactor): {},
-        ("efficiency", Percent): {},
-    },
-    "relations": [],
-}
-
 
 class Fan(Equipment):
     """
     A fan is composed of a blower and an electrical motor
-
-    Fan speed is an important property
-    The intrinsic property I want to make `actuatedby` a starter or a VFD, I'd like it to be
-    a PercentCommand.
-    Because RPM, cfm, amps, etc... will all depend of this and I would not want having to make multiple
-    `actuatesProperty` relationships between the starter/vfd and the fan.
-    I only want 1 property.
-    Then if required, it'll be possible to calculate RPM, amps, cfm, etc, based on the `PercentCommand` actuated property
-
-    RPM of the fan, can be different from RPM of motor (ex. belt and pulley)
-    So here, speed_ratio != RPM because if a fan can deliver x RPM, the ratio of pulley can be changed and
-    giving 100% speed_ratio to a fan, may mean a different speed than RPM of fan.
-
-    A motor starter will give 0% or 100% of speed_ratio depending on being On or Off
-    A VFD will modulate this speed ratio from 0% to 100%.
-    Depending on the configuration of the drive, this ratio can represent 0-60Hz... or 20-50Hz
-    (if freq are configured different than 0-60Hz), etc...
-
-    Using speed_ratio will isolate the model from the real time data by providing an agnostic way
-    of actuating the fan.
     """
 
     _class_iri: URIRef = S223.Fan
+    #electricalInlet: ElectricalInletConnectionPoint
     airInlet: AirInletConnectionPoint
     airOutlet: AirOutletConnectionPoint
-    onOffStatus: PropertyReference
-    onOffCommand: PropertyReference
 
     def __init__(self, config: Dict = None, **kwargs):
-        _config = template_update(fan_template, config=config)
+        _config = template_update({}, config=config)
         kwargs = {**_config.pop("params", {}), **kwargs}
-        _log.debug(f"Fan.__init__ {_config} {kwargs}")
-
+        _log.info(f"Fan.__init__ {_config} {kwargs}")
+        _relations = _config.pop("relations", [])
         super().__init__(_config, **kwargs)
-        configure_relations(self, _config.get("relations", []))
+        configure_relations(self, _relations)
 
-
-system_fan_template = {
-    "equipment": {
-        ("fan", Fan): {
-            "config": {
-                "cp": {
-                    "electricalInlet": Electricity_600VLL_3Ph_60HzInletConnectionPoint
-                },
-                "properties": {
-                    ("speedRatio", PercentCommand): {},
-                    ("staticPressure", Pressure): {"hasUnit": UNIT.PA},
-                    ("amps", Amps): {},
-                    ("rpm", RPM): {},
-                    ("cfm", Flow): {"hasUnit": UNIT["FT3-PER-MIN"]},
-                    ("hp", HP): {},
-                    ("kW", ElectricPowerkW): {},
-                    ("powerFactor", PowerFactor): {},
-                    ("efficiency", Percent): {},
-                },
-                "relations": [],
-            }
-        }
-    }
-}
-
-
-starter_addon_template = {
-    "equipment": {
-        ("starter", MotorStarter): {
-            "config": {
-                "cp": {
-                    "electricalInlet": Electricity_600VLL_3Ph_60HzInletConnectionPoint,
-                    "electricalOutlet": Electricity_600VLL_3Ph_60HzOutletConnectionPoint,
-                },
-                "properties": {("speedRatio", PercentCommand): {}},
-            },
-        }
-    },
-    "relations": [
-        ("self['fan'].onOffCommand", "=", 'self["starter"]["onOffCommand"]'),
-        (
-            "self['fan'].onOffStatus",
-            "=",
-            'self["starter"]["currentRelay"]["currentSensor"].observes',
-        ),
-        ('self["starter"].actuatesProperty', "=", 'self["fan"]["speedRatio"]'),
-        ('self["starter"].electricalOutlet', ">>", "self['fan'].electricalInlet"),
-    ],
-}
-
-
-fan_with_starter_template = template_update(system_fan_template, starter_addon_template)
-
-
-VFD_addon_template = {
-    "equipment": {("vfd", VFD): {}},
-    "properties": {},
-    "relations": [
-        ("self['fan'].onOffCommand", "=", 'self["vfd"]["run_command"]'),
-        ("self['fan'].onOffStatus", "=", 'self["vfd"]["drive_running"]'),
-        ('self["vfd"].actuatesProperty', "=", 'self["fan"]["speedRatio"]'),
-        ('self["vfd"].electricalOutlet', ">>", "self['fan'].electricalInlet"),
-    ],
-}
-
-fan_with_vfd_template = template_update(system_fan_template, VFD_addon_template)
