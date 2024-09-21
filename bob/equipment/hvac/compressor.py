8bf93d077c09f64aa547ad223930f42bd572c2d9
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-09-20 22:43:56 -0400

    Needed a compressor

--- bob/equipment/hvac/compressor.py
+++ bob/equipment/hvac/compressor.py
@@ -11,7 +11,17 @@ from ...connections.electricity import (
     ElectricalOutletConnectionPoint,
     Electricity_600VLL_3Ph_60HzInletConnectionPoint,
 )
+from ...connections.refrigerant import (
+    RefrigerationGasInletConnectionPoint,
+    RefrigerationGasOutletConnectionPoint,
+)
 from ...core import BOB, P223, S223, Equipment, PropertyReference
+from ...enum import (  # , R134a, R404a, R407c, R448a, R449a, R452a, R454b, R507a
+    R22,
+    R32,
+    R410a,
+    RefrigerationGas,
+)
 
 _namespace = BOB
 
@@ -40,3 +50,28 @@ class AirCompressor(Equipment):
         )
         kwargs = {**config.get("params", {}), **kwargs}
         super().__init__(config, **kwargs)
+
+
+class RefrigeartionGasCompressor(Equipment):
+    _class_iri: URIRef = S223.Compressor
+    returnPort: RefrigerationGasInletConnectionPoint
+    dischargePort: RefrigerationGasOutletConnectionPoint
+
+    onOffStatus: OnOffStatus
+    alarmStatus: NormalAlarmStatus
+    onOffCommand: OnOffCommand
+
+    # This will come from a sensor, but accessible from here
+    dischargePressure: PropertyReference
+    succionPressure: PropertyReference
+
+    def __init__(self, config: Dict = compressor_template, **kwargs):
+        config["properties"] = config.get(
+            "properties", compressor_template["properties"]
+        )
+        kwargs = {**config.get("params", {}), **kwargs}
+        super().__init__(config, **kwargs)
+
+    def set_gas_type(self, gas: RefrigerationGas):
+        self.returnPort.hasMedium = gas
+        self.dischargePort.hasMedium = gas
