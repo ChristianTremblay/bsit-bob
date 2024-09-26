b0a281223f253a94755d8090b0bfde647356c26e
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-09-26 14:31:38 -0400

    Cleanup required as ontoenv was containing old invalid stuff... weirest is the VOCAB file found inside tool that I used to define electricity at first

--- validate_cicd/test_validation.py
+++ validate_cicd/test_validation.py
@@ -11,8 +11,9 @@ from pathlib import Path
 import ontoenv
 import rdflib
 from dotenv import load_dotenv
+from topquadrant_shacl import validate
 
-from .topquadrant_shacl import validate
+from bob.core import dump
 
 load_dotenv()
 S223_FOLDER = Path(os.getenv("S223_FOLDER"))
@@ -29,22 +30,41 @@ def copy_graph(g: rdflib.Graph) -> rdflib.Graph:
     return c
 
 
+def create_schema():
+    """
+    Creates the schema definition against the specified shapes
+    """
+    # load in schema validation shapes
+    shape_graph = rdflib.Graph()
+    print("Loading shapes from", S223_FOLDER / "validation")
+    for shape_file in glob.glob(str(S223_FOLDER / "validation" / "*.ttl")):
+        print("    Loading", shape_file)
+        shape_graph.parse(shape_file, format="turtle")
+    print("Loading shapes from", S223_FOLDER / "inference")
+    for shape_file in glob.glob(str(S223_FOLDER / "inference" / "*.ttl")):
+        print("    Loading", shape_file)
+        shape_graph.parse(shape_file, format="turtle")
+    print("Loading shapes from", S223_FOLDER / "models")
+    for shape_file in glob.glob(str(S223_FOLDER / "models" / "*.ttl")):
+        print("    Loading", shape_file)
+        shape_graph.parse(shape_file, format="turtle")
+    print("Loading shapes from", S223_FOLDER / "vocab")
+    for shape_file in glob.glob(str(S223_FOLDER / "vocab" / "*.ttl")):
+        print("    Loading", shape_file)
+        shape_graph.parse(shape_file, format="turtle")
+    print("Loading shapes from", S223_FOLDER / "imports")
+    for shape_file in glob.glob(str(S223_FOLDER / "imports" / "**" / "*.ttl")):
+        print("    Loading", shape_file)
+        shape_graph.parse(shape_file, format="turtle")
+    return shape_graph
+
+
 def test_schema_validation():
     """
     Validates the schema definition against the specified shapes
     """
     # load in schema validation shapes
-    shape_graph = rdflib.Graph()
-    for shape_file in glob.glob(str(S223_FOLDER / "validation" / "*.ttl")):
-        shape_graph.parse(shape_file, format="turtle")
-    for shape_file in glob.glob(str(S223_FOLDER / "inference" / "*.ttl")):
-        shape_graph.parse(shape_file, format="turtle")
-    for shape_file in glob.glob(str(S223_FOLDER / "models" / "*.ttl")):
-        shape_graph.parse(shape_file, format="turtle")
-    for shape_file in glob.glob(str(S223_FOLDER / "vocab" / "*.ttl")):
-        shape_graph.parse(shape_file, format="turtle")
-    for shape_file in glob.glob(str(S223_FOLDER / "imports" / "**" / "*.ttl")):
-        shape_graph.parse(shape_file, format="turtle")
+    shape_graph = create_schema()
 
     # import dependencies on other ontologies
     env = ontoenv.OntoEnv()
@@ -95,3 +115,22 @@ def test_data_validation(data_file):
     # save inferred graph under same name into data/compiled/
     inferred.serialize(data_file.parent / "compiled" / data_file.name, format="turtle")
     assert valid, report.serialize(format="ttl")
+
+
+def dump_graphs():
+    """
+    Dumps the schema and data graphs to ttl files
+    """
+    # load in all dependent data validation and model definition shapes
+    env = ontoenv.OntoEnv()
+    shape_graph = create_schema()
+    env.import_dependencies(shape_graph)
+    dump(
+        shape_graph,
+        filename="schema.ttl",
+        header="# Schema definition for the 223P repository",
+    )
+
+
+if __name__ == "__main__":
+    dump_graphs()
