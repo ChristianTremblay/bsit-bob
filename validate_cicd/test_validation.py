3ff27564af4f982297285f47a0ad308cdda426af
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-09-30 22:56:04 -0400

    Better reports when validating and always create artefact so we get access to reports

--- validate_cicd/test_validation.py
+++ validate_cicd/test_validation.py
@@ -25,7 +25,7 @@ S223_FOLDER = Path(os.getenv("S223_FOLDER"))
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
+schema_report = None
 
 def copy_graph(g: rdflib.Graph) -> rdflib.Graph:
     c = rdflib.Graph()
@@ -77,6 +77,8 @@ def test_schema_validation():
     logger.info("Validating schema definition")
     # validate with topquadrant shacl
     report, valid, _ = validate(shape_graph)
+    global schema_report
+    schema_report = report
 
     # valid, _, res_text = pyshacl.validate(data_graph=shape_graph, advanced=True, allow_warnings=True)
     assert (
@@ -118,6 +120,19 @@ def test_data_validation(data_file):
     (data_file.parent / "compiled").mkdir(exist_ok=True)
     # save inferred graph under same name into data/compiled/
     inferred.serialize(data_file.parent / "compiled" / data_file.name, format="turtle")
+    report.serialize(data_file.parent / "compiled" / f"{data_file.stem}.validation_report.ttl", format="turtle")
+    schema_report.serialize(data_file.parent / "compiled" / f"{data_file.stem}.schema_report.ttl", format="turtle")
+    def rdf_to_html_table(graph):
+        html = "<html><body><table border='1'>"
+        html += "<tr><th>Subject</th><th>Predicate</th><th>Object</th></tr>"
+
+        for subj, pred, obj in graph:
+            html += f"<tr><td>{subj}</td><td>{pred}</td><td>{obj}</td></tr>"
+
+        html += "</table></body></html>"
+        with open(data_file.parent / "compiled" / f"{data_file.stem}.validation_report.html", "w") as f:
+            f.write(html)
+    rdf_to_html_table(report)
     assert valid, report.serialize(format="ttl")
 
 
