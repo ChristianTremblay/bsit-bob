281e49bf747e53046238b23cdccc655853b9f3b3
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-10-14 16:54:31 -0400

    debug scripts are now in doc folder

---
+++ doc/debug-001.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+print(f"{os.getenv('BOB_LOG') = }")
+
+logging.basicConfig(level=logging.WARNING)
+print(f"{logging.root} level={logging.root.level} handlers={logging.root.handlers}")
+
+x = logging.getLogger("bob")
+print(f"{x} level={x.level} handlers={x.handlers}")
+
+y = logging.getLogger("bob.core")
+print(f"{y} level={y.level} handlers={y.handlers}")
+print()
+
+import bob
+
+print(f"{x} level={x.level} handlers={x.handlers}")
+print(f"{y} level={y.level} handlers={y.handlers}")
+print("----------")
+
+from bob.equipment.hvac.valve import TwoWayActuatedOnOffValve
+
+valve = TwoWayActuatedOnOffValve(label="valve")
+print(f"{valve = }")
