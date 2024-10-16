de239f3a7ce4cc45186b919423b9ded6acd10a90
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-10-15 22:17:57 -0400

    improvements to query_model

--- ttl_tools/query_model.py
+++ ttl_tools/query_model.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 import rdflib
 
+from bob.core import clear
+
 
 def load_query(query_file):
     with open(query_file, "r") as file:
@@ -28,6 +30,7 @@ def main():
         "--move",
         action="store_true",
         help="Move the output file to a different directory",
+        default=True,
     )
 
     args = parser.parse_args()
@@ -58,12 +61,32 @@ def query(model: rdflib.Graph = None, sparql: Path = None, move=False):
     with open(report_file.resolve(), "w") as output_file:
         output_file.write("<html><body>\n")
         output_file.write("<h1>SPARQL Query</h1>\n")
+        output_file.write("<h2>{}</h2>\n".format(_name))
         output_file.write("<pre>{}</pre>\n".format(query))
         output_file.write("<h1>Results</h1>\n")
         output_file.write("<table border='1'>\n")
         output_file.write("<tr><th>Result</th></tr>\n")
         for row in results:
-            output_file.write("<tr><td>{}</td></tr>\n".format(row))
+            output_file.write("<tr>\n")
+            for var in row:
+                if isinstance(var, rdflib.term.URIRef):
+                    value = model.namespace_manager.normalizeUri(var)
+                    output_file.write(f"<td>{str(value)}</td>")
+                elif ", " in var:
+                    value = var.split(", ")
+                    for each in value:
+                        if isinstance(var, rdflib.term.URIRef) or "http" in each:
+                            value = model.namespace_manager.normalizeUri(each)
+                            output_file.write(f"<td>{str(value)}</td>")
+                        else:
+                            value = str(each)
+                            output_file.write(f"<td>{str(value)}</td>")
+                else:
+                    value = str(
+                        var
+                    )  # Fallback to the full URI if it cannot be normalized
+                    output_file.write(f"<td>{str(value)}</td>")
+            output_file.write("</tr>\n")
         output_file.write("</table>\n")
         output_file.write("</body></html>\n")
 
