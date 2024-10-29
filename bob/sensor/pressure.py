6fac690d8a7f6254c48664420c6b7c21d458313f
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-10-29 15:38:34 -0400

    Fixing pressure sensor. Must provide observation and reference, but I didn't want to loose the function behind, that subtract high - low

--- bob/sensor/pressure.py
+++ bob/sensor/pressure.py
@@ -1,10 +1,11 @@
-from typing import Any, List
+from typing import Any, List, Tuple
 
 from rdflib import URIRef
 
 from bob import core
 from bob.connections.controlsignal import OnOffSignalOutletConnectionPoint
 from bob.producer.causality import Differential
+from bob.properties.force import DifferentialStaticPressure, Pressure
 
 from ..core import (
     BOB,
@@ -44,26 +45,11 @@ class PressureSensor(Sensor):
 
         super().__init__(**_sensor_kwargs)
 
-        self.observes = DifferentialStaticPressure(
+        self.observes = Pressure(
             # isObservedBy=self,
-            label=f"{self.label}.DifferentialStaticPressure",
+            label=f"{self.label}.GaugePressure",
             **_property_kwargs,
         )
-
-
-class PressureReferenceSensor(PressureSensor):
-    _class_iri = S223.PressureSensor
-
-    def add_hasObservationLocation(self, node: Node) -> None:
-        # This is a special case called hasReferenceLocation
-        # but we keep the same function name for consistency
-        # and make % work
-        # link the two together
-        self._data_graph.add(
-            (self._node_iri, S223.hasReferenceLocation, node._node_iri)
-        )
-        if INCLUDE_INVERSE:
-            node.isReferenceLocation = self
 
 
 # class DifferentialStaticPressureSetpoint(Setpoint):
@@ -74,28 +60,47 @@ class PressureReferenceSensor(PressureSensor):
 
 class DifferentialStaticPressureSensor(Sensor):
     _class_iri = S223.PressureSensor
+    observes: PropertyReference
+    observation_pressure: Pressure
+    reference_pressure: Pressure
+    differential_static_pressure: DifferentialStaticPressure
+    highPort: PressureSensor
+    lowPort: PressureSensor
 
     def __init__(self, **kwargs: Any) -> None:
         _sensor_kwargs, _property_kwargs = split_kwargs(kwargs)
 
         super().__init__(**_sensor_kwargs)
 
-    def add_hasObservationLocation(self, node: Node) -> None:
+    def add_hasObservationLocation(self, node: Tuple[Node, Node]) -> None:
         # For now, make that a secret, or we end up with s223.hasObservationLocation
         # self._hasObservationLocation = node
 
         # link the two together
+        observation_location, reference_location = node
         self._data_graph.add(
-            (self["highPort"]._node_iri, S223.hasReferenceLocation, node._node_iri)
+            (self._node_iri, S223.hasReferenceLocation, reference_location._node_iri)
         )
         if INCLUDE_INVERSE:
-            node.isReferenceLocation = self["highPort"]
+            reference_location.isReferenceLocation = self
+
+        self._data_graph.add(
+            (
+                self._node_iri,
+                S223.hasObservationLocation,
+                observation_location._node_iri,
+            )
+        )
+        if INCLUDE_INVERSE:
+            observation_location.isReferenceLocation = self
+
+        self["highPort"] % observation_location
+        self["lowPort"] % reference_location
 
 
 class AirDifferentialStaticPressureSensor(DifferentialStaticPressureSensor):
     _class_iri = S223.PressureSensor
     # observes: PropertyReference
-    differential_static_pressure: DifferentialStaticPressure
 
     def __init__(self, **kwargs):
         _sensor_kwargs, _property_kwargs = split_kwargs(kwargs)
@@ -105,22 +110,32 @@ class AirDifferentialStaticPressureSensor(DifferentialStaticPressureSensor):
             label=f"{self.label}.DifferentialStaticPressure",
             **_property_kwargs,
         )
-        self > PressureSensor(label=f"highPort", ofMedium=Air, **_property_kwargs)
-        self > PressureReferenceSensor(
-            label=f"lowPort", ofMedium=Air, **_property_kwargs
+        self.observation_pressure = Pressure(
+            ofMedium=Air,
+            label=f"{self.label}.ObservationPressure",
+            **_property_kwargs,
+        )
+        self.reference_pressure = Pressure(
+            ofMedium=Air,
+            label=f"{self.label}.ReferencePressure",
+            **_property_kwargs,
         )
         self > Differential(
             label="diff_causality", comment="Will output High minus Low"
         )
-        self["highPort"].observedProperty >> self["diff_causality"].high_input
-        self["lowPort"].observedProperty >> self["diff_causality"].low_input
+        self > PressureSensor(label=f"highPort", ofMedium=Water, **_property_kwargs)
+        self > PressureSensor(label=f"lowPort", ofMedium=Water, **_property_kwargs)
+        self > Differential(label="output", comment="Will output High minus Low")
+        # self["highPort"].observes >> self.observation_pressure
+        # self["lowPort"].observes >> self.reference_pressure
+        self.observation_pressure >> self["diff_causality"].high_input
+        self.reference_pressure >> self["diff_causality"].low_input
         self["diff_causality"].differential_output >> self.differential_static_pressure
         self.observes = self.differential_static_pressure
 
 
 class WaterDifferentialStaticPressureSensor(DifferentialStaticPressureSensor):
     _class_iri = S223.DifferentialSensor
-    differential_static_pressure: DifferentialStaticPressure
 
     def __init__(self, **kwargs):
         _sensor_kwargs, _property_kwargs = split_kwargs(kwargs)
@@ -130,10 +145,25 @@ class WaterDifferentialStaticPressureSensor(DifferentialStaticPressureSensor):
             label=f"{self.label}.DifferentialStaticPressure",
             **_property_kwargs,
         )
-        self > PressureSensor(label=f"highPort", ofMedium=Water, **_property_kwargs)
-        self > PressureSensor(label=f"lowPort", ofMedium=Water, **_property_kwargs)
+        self.observation_pressure = Pressure(
+            ofMedium=Water,
+            label=f"{self.label}.ObservationPressure",
+            **_property_kwargs,
+        )
+        self.reference_pressure = Pressure(
+            ofMedium=Water,
+            label=f"{self.label}.ReferencePressure",
+            **_property_kwargs,
+        )
+        self > Differential(
+            label="diff_causality", comment="Will output High minus Low"
+        )
+        self > PressureSensor(label="highPort", ofMedium=Water, **_property_kwargs)
+        self > PressureSensor(label="lowPort", ofMedium=Water, **_property_kwargs)
         self > Differential(label="output", comment="Will output High minus Low")
-        self["highPort"].observes >> self["output"].high
-        self["lowPort"].observes >> self["output"].low
-        self["output"].differential >> self.differential_static_pressure
+        # self["highPort"].observes >> self.observation_pressure
+        # self["lowPort"].observes >> self.reference_pressure
+        self.observation_pressure >> self["diff_causality"].high_input
+        self.reference_pressure >> self["diff_causality"].low_input
+        self["diff_causality"].differential_output >> self.differential_static_pressure
         self.observes = self.differential_static_pressure
