8e0ffe4083dd4674827bc6bc17c97918cc916efe
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-12-17 22:31:23 -0500

    qudt path changed

--- bob/core.py
+++ bob/core.py
@@ -223,8 +223,8 @@ _namespace = S223
 
 # common namespaces
 QUDT = bind_namespace("qudt", "http://qudt.org/schema/qudt/")
-QUANTITYKIND = bind_namespace("qudtqk", "http://qudt.org/vocab/quantitykind/")
-UNIT = bind_namespace("unit", "http://qudt.org/vocab/unit/")
+QUANTITYKIND = bind_namespace("qudtqk", "http://qudt.org/schema/qudt/QuantityKind/")
+UNIT = bind_namespace("unit", "http://qudt.org/schema/qudt/Unit/")
 BRICK = bind_namespace("brick", "https://brickschema.org/schema/Brick#")
 
 # the model_namespace is used to create "blank" node identifiers, a serial
