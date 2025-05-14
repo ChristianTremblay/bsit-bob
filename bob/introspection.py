e7a1532920b53e82260f0c4fae6302c2acac43d9
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2025-05-14 16:38:45 -0400

    UNIT are a special case being enumeration kind, I can short circuit introspection here

--- bob/introspection.py
+++ bob/introspection.py
@@ -4,6 +4,7 @@ import pkgutil
 import typing as t
 
 import bob
+from .core import UNIT
 
 class_cache = {}
 module_cache = {}
@@ -35,7 +36,7 @@ def load_modules(module: t.Type = bob, force: bool = False) -> None:
 def get_class_from_name(classname: str = None, module: t.Type = bob) -> t.Type:
     global class_cache
     global module_cache
-    super = None
+    _super = None
     if not classname:
         raise ValueError("Classname is required")
 
@@ -44,7 +45,10 @@ def get_class_from_name(classname: str = None, module: t.Type = bob) -> t.Type:
         return _existing
 
     if "." in classname:
-        super, classname = classname.split(".")
+        _super, classname = classname.split(".")
+        if _super == "UNIT":
+            return UNIT[classname]
+
     if "|" in classname:
         _module, classname = classname.split("|")
         module = importlib.import_module(_module)
@@ -64,8 +68,8 @@ def get_class_from_name(classname: str = None, module: t.Type = bob) -> t.Type:
 
             if inspect.isclass(obj):
                 if name == classname:
-                    if super is not None:
-                        _key = f"{super}.{classname}"
+                    if _super is not None:
+                        _key = f"{_super}.{classname}"
                     else:
                         _key = classname
                     class_cache[_key] = obj
@@ -73,13 +77,13 @@ def get_class_from_name(classname: str = None, module: t.Type = bob) -> t.Type:
                 else:
                     if name not in class_cache:
                         class_cache[name] = obj
-        if super is not None:
-            if super in enum_cache:
-                _super_class = enum_cache[super]
+        if _super is not None:
+            if _super in enum_cache:
+                _super_class = enum_cache[_super]
                 return getattr(_super_class, classname)
             else:
                 if classname in enum_cache:
                     return enum_cache[classname]
     raise TypeError(
-        f"Class {super} {classname} not found in {module}, cache is {class_cache}"
+        f"Class {_super} {classname} not found in {module}, cache is {class_cache}"
     )
