8a544f428e24c9d0a618ad323f5c9deb66030f80
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2025-06-27 14:20:18 -0400

    Cleanup starter

--- bob/equipment/electricity/__init__.py
+++ bob/equipment/electricity/__init__.py
@@ -4,15 +4,6 @@ from bob.equipment.control.controller import Controller
 _namespace = S223
 
 
-class _MotorStarter(Equipment):
-    """
-    This is required here so actuatesProperty gets its namespace from S223
-    """
-
-    _class_iri = S223.Equipment
-    actuatesProperty: Property
-
-
 class _VFD(Controller):
     """
     This is required here so actuatesProperty gets its namespace from S223
