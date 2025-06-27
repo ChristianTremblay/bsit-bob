89aa951df607169b75d3969b8ce836b800c85765
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2025-06-27 14:56:42 -0400

    Fixing test as some complex equipment were moved to si-modeler

--- tests/test_device-002.py
+++ tests/test_device-002.py
@@ -3,11 +3,7 @@ from pathlib import Path
 from header import ttl_test_header
 
 from bob.core import bind_model_namespace, dump
-from bob.equipment.hvac.boiler import (
-    ElectricalHotWaterBoiler,
-    HotWaterBoiler,
-    NaturalGasHotWaterBoiler,
-)
+from bob.equipment.hvac.boiler import Boiler
 from bob.equipment.hvac.coil import (
     ChilledWaterCoil,
     ElectricalHeatingCoil,
@@ -19,13 +15,7 @@ _namespace = bind_model_namespace("ex", f"urn:ex/{model_name}/")
 
 
 def test_create_more_complex_Equipment(bob_fixture):
-    boiler = HotWaterBoiler(label="HWB-1", comment="Hot Water Boiler")
-    electrical_hot_water_boiler = ElectricalHotWaterBoiler(
-        label="Electrical Hot Water Boiler"
-    )
-    naturalgas_hot_water_boiler = NaturalGasHotWaterBoiler(
-        label="HWB-2", comment="Natural Gas Hot Water Boiler"
-    )
+    boiler = Boiler(label="HWB-1", comment="Hot Water Boiler")
 
     hot_water_coil = HotWaterCoil(label="Hot Water Coil")
     chilled_water_coil = ChilledWaterCoil(label="Chilled Water Coil")
