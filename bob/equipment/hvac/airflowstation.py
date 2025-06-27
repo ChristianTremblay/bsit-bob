07fd6e6cb06405d2ac40a9468cc00e35e977d2d6
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2025-06-27 11:55:04 -0400

    AirFlowMonitor is not a s223.FlowSensor. It is an equipment containing a sensor

--- bob/equipment/hvac/airflowstation.py
+++ bob/equipment/hvac/airflowstation.py
@@ -1,15 +1,15 @@
 from ...connections.air import AirInletConnectionPoint, AirOutletConnectionPoint
-from ...core import BOB, S223, Equipment
-from ...sensor import Sensor
+from ...core import BOB, S223, Equipment, P223
+from ...sensor.flow import AirFlowSensor
 
 _namespace = BOB
 
 
 class AirFlowMonitor(Equipment):
-    _class_iri = S223.FlowSensor
+    _class_iri = P223.AirFlowMonitor
     airInlet: AirInletConnectionPoint
     airOutlet: AirOutletConnectionPoint
-    flowSensor: Sensor
+    flowSensor: AirFlowSensor
 
 
 # TODO : Create the template and make that the same than the others.
